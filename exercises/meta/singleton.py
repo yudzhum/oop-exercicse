f110336f788134992dc3f83376feaebc1ad19877
Yuliya Dzhumanazarova <yaschick451@gmail.com>
2024-01-23 21:52:24 +0300

    add more

--- exercises/meta/singleton.py
+++ exercises/meta/singleton.py
@@ -1,12 +1,12 @@
 def singleton(cls):
-    class NewClass:
-        isinstances = []
+    instances = {}
 
-        def __init__(self, *args, **kwargs):
-            self.obj = cls(*args, **kwargs)
-
-    return NewClass
-
+    def wrapper(*args, **kwargs):
+        if cls not in instances:
+            instances[cls] = cls(*args, **kwargs)
+        return instances[cls]
+    return wrapper
+        
 
 @singleton
 class Logger:
